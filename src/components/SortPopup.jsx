8785fafbede91a206fc401589f94767eb0453075
Voyager101pw <voyager101sirius@gmail.com>
2022-08-19 14:02:00 +0500

    Added popup feature for sort component

--- src/components/SortPopup.jsx
+++ src/components/SortPopup.jsx
@@ -8,12 +8,30 @@ function SortPopup() {
   // const currentSort = useSelector(selectCurrentSort)
   const currentSort = 'популярности';
 
+  // const popupIsOpen = useSelector(selectPopupStatus);
+  const popupIsOpen = true;
+
   return (
-    <div className="sort">
-      <img src={vector} />
-      <b>Сортировка по:</b>
-      <span>{currentSort}</span>
-    </div>
+    <>
+      <div className="sort">
+        <img src={vector} />
+        <b>Сортировка по:</b>
+        <span>{currentSort}</span>
+      {
+        popupIsOpen
+          ? (
+          <div className="sort__popup">
+            <ul>
+              <li className="active">популярности</li>
+              <li>цене</li>
+              <li>алфавиту</li>
+            </ul>
+          </div>
+          )
+          : null
+        }
+      </div>
+  </>
   );
 }
 
