77823f6dfa841a0f5d9f09dce7cbc72bb67cc281
tester <test@example.com>
2022-10-02 01:04:14 +0500

    Added navigation for cart

--- src/components/Buttons/GetBackBtn.jsx
+++ src/components/Buttons/GetBackBtn.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import cn from 'classnames';
-
+import { useNavigate } from 'react-router-dom';
 import BackIcon from '../../assets/img/arrow-back.svg';
 
 function GetBackBtn({ className }) {
+  const navigate = useNavigate();
+
   const classes = cn('btn btn--outline btn--gray cart__btn-back', className);
   return (
-    <div className={classes}>
+    <div className={classes} onClick={() => navigate('/')}>
       <BackIcon className="btn__back-icon" />
       Вернуться назад
     </div>
